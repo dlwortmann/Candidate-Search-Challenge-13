080e420b9f7645c6e63bc6cd957665fd929f36f8
dlwortmann <dannywortmann@gmail.com>
2025-03-21 18:40:00 -0500

    add git token to .env and start buidling Candidate interface

--- Develop/src/interfaces/Candidate.interface.tsx
+++ Develop/src/interfaces/Candidate.interface.tsx
@@ -1,1 +1,12 @@
 // TODO: Create an interface for the Candidate objects returned by the API
+interface Candidate {
+    name: ;
+    username: ;
+    location: ;
+    avatar: ;
+    email: ;
+    html_url: ;
+    company: ;
+}
+
+export default Candidate;
