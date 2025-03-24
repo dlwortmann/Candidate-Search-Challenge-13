854a563a2746d5c4c7d55e6a40a25f40efab6fdf
dlwortmann <dannywortmann@gmail.com>
2025-03-23 21:08:13 -0500

    updates to SavedCandidates and Candidate search for setting local storage

--- Develop/src/pages/CandidateSearch.tsx
+++ Develop/src/pages/CandidateSearch.tsx
@@ -1,7 +1,9 @@
 import { useState, useEffect } from 'react';
 import { searchGithub, searchGithubUser } from '../api/API';
 import Candidate from '../interfaces/Candidate.interface';
-
+import { FormEvent } from 'react';
+import CandidateCard from '../components/CandidateCard';
+// useEffect and searchGithub were in starter code, were should the be included?
 const CandidateSearch = () => {
   const [currentCandidate, setCurrentCandidate] = useState<Candidate>({
     name: '',
@@ -12,13 +14,46 @@ const CandidateSearch = () => {
     html_url: '',
     company: '',
   })
-  
+
   const [searchInput, setSearchInput] = useState<string>('');
 
   const addToSavedCandidates = () => {
-    
+    let parsedSavedCandidates: Candidate[] = [];
+    const storedCandidates = localStorage.getItem('savedCandidates');
+    if (typeof storedCandidates === 'string') {
+      parsedSavedCandidates = JSON.parse(storedCandidates)
+    }
+    parsedSavedCandidates.push(currentCandidate);
+    localStorage.setItem('savedCandidate', JSON.stringify(parsedSavedCandidates))
+  };
+
+  const searchForCandidates = async (event: FormEvent, candidate: string) => {
+    event.preventDefault();
+    const data: Candidate = await searchGithubUser(candidate);
+
+    setCurrentCandidate(data);
   }
-  return <h1>CandidateSearch</h1>;
+
+  return (
+    <>
+      <h1>CandidateSearch</h1>;
+      <section>
+        <form onSubmit={(event: FormEvent) => searchForCandidates(event, searchInput)}>
+          <input type='text'
+            name=''
+            id=''
+            placeholder='Enter a name'
+            onChange={(e) => setCurrentCandidate(searchInput)} />;
+          <button type='submit' id='searchBtn'>
+            Search
+          </button>
+        </form>
+      </section>
+      <CandidateCard
+        currentCandidate={currentCandidate}
+        addToSavedCandidateList={addToSavedCandidates} />
+    </>
+  )
 };
 
 export default CandidateSearch;
--- Develop/src/pages/SavedCandidates.tsx
+++ Develop/src/pages/SavedCandidates.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
 import SavedCandidateList from '../components/SavedCandidateList.tsx'
+import Candidate from '../interfaces/Candidate.interface.tsx';
 const SavedCandidates = () => {
+  const [savedCandidates, setSavedCandidates] = useState<Candidate[]>([]);
+
+  useEffect(() => {
+    const parsedSavedCandidates = JSON.parse(localStorage.getItem('savedCandidate') as string);
+    setSavedCandidates(parsedSavedCandidates)
+  }, [])
   return (
     <>
       <h1>Potential Candidates</h1>
+      {(!savedCandidates?.length || savedCandidates?.length === 0) ? (<h1>Add Candidates to list</h1>) : (<SavedCandidateList savedCandidates={savedCandidates}
+      />
+      )}
     </>
   );
 };
